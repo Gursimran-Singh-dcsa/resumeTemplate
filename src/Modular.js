1d1688972e30520daba0decd08e11ae3a86e35d7
gursimran singh <gursimransingh8075@gmail.com>
2020-05-20 23:54:16 +0530

    make table editable

--- src/Modular.js
+++ src/Modular.js
@@ -19,8 +19,22 @@ const Simple = (props) => {
 }
 
 const GetTable = (props) => {
+  const [data, setdata] = useState(['', '', '', '']);
+  const savedata = (e) => {
+    props.onChange(prevState => {
+      return {...prevState, value: [...prevState.value, data], addMore: false }
+    })
+    console.log(props.rows)
+  }
+  const handleInputChange = (e) => {
+    e.persist();
+    setdata(prevState => {
+      prevState[e.target.name] = e.target.value
+      return prevState;
+    })
+  }
   return(
-    <table>
+    <table className="table">
       <thead>
         <tr>
           {props.headings.map(element => {
@@ -28,24 +42,37 @@ const GetTable = (props) => {
           })}
         </tr>
       </thead>
-      <tbody>
-        {props.rows.map(element => {
-          return(<tr key={element}>
+      <tbody id="academicdetails">
+        {props.rows.map((element, index) => {
+          return(<tr key={element+index}>
             {element.map(item => {
               return <td key={item}>{item}</td>
             })}
           </tr>)
         })}
+        {props.addmore ? <tr className="admorerow">
+          {console.log(data)}
+          {data.map((element, index) => {
+            return (<td key={index+element}><input type="text" name={index} onChange={handleInputChange} /></td>)
+          })}
+        </tr> : null}
+        {props.addmore ? <tr className="save">
+            <td colSpan={props.headings.length}><button onClick={savedata}>Save</button></td>
+        </tr> : null}
       </tbody>
     </table>
   )
 }
 
 const Tabular = (props) => {
+  const handleAddMore = () => {
+    props.onChange({...props.info, addMore: !props.info.addMore})
+  }
   return (
     <div>
       <div className="fieldheading">{props.info.name}</div>
-      <GetTable rows={(props.info.value)} headings={props.info.headings}/>
+      <GetTable rows={(props.info.value)} headings={props.info.headings} addmore={props.info.addMore} onChange={props.onChange}/>
+      <div><span id="addmore" onClick={handleAddMore}>+Addmore? </span></div>
       {/* <span>new column</span> */}
       {/* <div>new row</div> */}
     </div>
@@ -62,24 +89,28 @@ const Modular = () => {
     name: "Academic Qualifications",
     type: "Tabular",
     headings: ['Course', 'Board/Institution/University', 'Year of passing', 'Percentage'],
-    value: [['MCA', 'PU', '2020', '75']]
+    addMore: false,
+    value: [['MCA', 'PU', '2020', '75'], ['B.Sc', 'KUK', '2017', '70']]
   })
 
   const [experince, setExperice] = useState({
     name: "Work Experince",
     type: "repeatable",
+    addMore: false,
     value: []
   })
 
   const [strength, setStrength] = useState({
     name: "Strength",
     type: "repeatable",
+    addMore: false,
     value: []
   })
 
   const [skills, setSkills] = useState({
     name: "Skills",
     type: "repeatable",
+    addMore: false,
     value: []
   })
 
